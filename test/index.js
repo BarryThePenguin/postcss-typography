735c6f05a37d177243ae3b79fa1d0cac8ad23032
Jonathan Haines <jonno.haines@gmail.com>
2017-05-14 14:19:25 +1000

    fix tests

--- test/index.js
+++ test/index.js
@@ -12,6 +12,28 @@ const ROOT = join(__dirname, 'fixtures');
 
 const fixtures = fs.readdirSync(ROOT);
 
+function prettify() {
+	return function (css) {
+		css.walkRules((rule, index) => {
+			if (index !== 0) {
+				rule.raws.before = '\n';
+			}
+
+			rule.raws.between = ' ';
+			rule.raws.after = '\n';
+		});
+
+		css.walkDecls((decl, index) => {
+			if (index !== 0) {
+				decl.raws.before = '\n';
+			}
+
+			decl.raws.before = '\n    ';
+			decl.raws.between = ': ';
+		});
+	};
+}
+
 test('typography()', t => {
 	t.is(typeof typography, 'function', 'should be a function');
 
@@ -31,7 +53,7 @@ test('fixtures', t => {
 		const input = read(join(filepath, 'input.css'), 'utf-8');
 		// eslint-disable-next-line import/no-dynamic-require
 		const config = require(join(filepath, 'config.js'));
-		const processor = postcss([typography(config)]);
+		const processor = postcss([typography(config), prettify()]);
 		const result = processor.process(input).css;
 
 		t.deepEqual(result, output, 'should work on `' + fixture + '`');
